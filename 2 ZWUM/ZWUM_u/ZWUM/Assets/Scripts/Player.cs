1518d082a2ec113aa31b61a4f939f1dbf7b64f03
Danny Hawk <HawkDanny@users.noreply.github.com>
2019-02-09 20:38:15 -0500

    Added some more movement code

---
+++ 2 ZWUM/ZWUM_u/ZWUM/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public float skatingForce;
+    public float rotationForce;
+    public float maxSpeed;
+    public float maxRadiansDelta;
+
+
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = this.transform.GetChild(0).GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 desiredForce = _rb.transform.up * skatingForce;
+
+        _rb.velocity = Vector2.ClampMagnitude(_rb.velocity, maxSpeed);
+
+        if (Input.GetKey(leftKey) && Input.GetKey(rightKey))
+            return;
+
+        if (Input.GetKey(leftKey))
+        {
+            _rb.AddForce(Vector3.RotateTowards(desiredForce, -_rb.transform.right, maxRadiansDelta, 0f));
+            _rb.AddTorque(rotationForce);
+        }
+
+        if (Input.GetKey(rightKey))
+        {
+            _rb.AddForce(Vector3.RotateTowards(desiredForce, _rb.transform.right, maxRadiansDelta, 0f));
+            _rb.AddTorque(-rotationForce);
+        }
+    }
+}
